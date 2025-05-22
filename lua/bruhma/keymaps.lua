5b525e0924667b461eec33280324176f8fa98d67
fordashton3 <fordashton3@gmail.com>
2025-05-22 11:48:20 -0500

    moved keymap to bruhma

--- lua/bruhma/keymaps.lua
+++ lua/bruhma/keymaps.lua
@@ -1,3 +1,10 @@
+-- NVIM Bindings
 vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Diagnostics
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+--
